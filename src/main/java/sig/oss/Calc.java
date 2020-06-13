416a5cfc452e69de8cab87ffc351dc8b341663ef
jhpark <jinsal69@pusan.ac.kr>
2020-06-14 00:22:42 +0900

    add inc and dec functions

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,12 +1,16 @@
 public class Calc {
-	public int r=0;
+	private int r=0;
 	public int add(int x, int y) { //add method
 		return x+y;
 	}
 	public int sub(int x, int y) { //add method
 		return x-y;
 	}
-	public void dec(int d){ 
-		r -= d;
+	public int inc(int d){
+		r += d;
 	}
+	public int getResult(){
+		return r;
+	}
+	
 }
